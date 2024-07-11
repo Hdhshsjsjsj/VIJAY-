2d65db8348274b98059ef6afe70776c5ec8336bd
Hdhshsjsjsj <ilashadam039@gmail.com>
2024-07-11 14:37:43 +0400

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -472,7 +472,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('⊗Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⊗', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
                     InlineKeyboardButton('👑 Bᴏᴛ Oᴡɴᴇʀ', callback_data="owner_info"),
-                    InlineKeyboardButton('⊙ Sᴜᴘᴘᴏʀᴛ Lɪꜱᴛ', url=GRP_LNK)
+                    InlineKeyboardButton('⊙ Sᴜᴘᴘᴏʀᴛ Lɪꜱᴛ', url='support')
                 ],[
                     InlineKeyboardButton('🛠 Hᴇʟᴘ', callback_data='help'),
                     InlineKeyboardButton('⌂ Aʙᴏᴜᴛ', callback_data='about'),
