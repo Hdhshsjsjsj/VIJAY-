a52bea99fea21a60fd0fa40bc840f33b575a1534
Hdhshsjsjsj <ilashadam039@gmail.com>
2024-07-11 14:03:26 +0400

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -482,12 +482,15 @@ async def cb_handler(client: Client, query: CallbackQuery):
                   ]]
 
         reply_markup = InlineKeyboardMarkup(buttons)
+        await client.edit_message_media(
+            query.message.chat.id, 
+            query.message.id, 
+            InputMediaPhoto(random.choice(PICS))
+        )
         await query.message.edit_text(
-            text=script.START_TXT.format(
-                query.from_user.mention, temp.U_NAME, temp.B_NAME
-            ),
+            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML,
+            parse_mode=enums.ParseMode.HTML
         )
         await query.answer("Piracy Is Crime")
     elif query.data == "help":
